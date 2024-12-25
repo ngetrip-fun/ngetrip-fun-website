76b840c918220ba42d73e26e993c3f8ea071c146
novinbukannopin <novinardiany@outlook.com>
2024-12-25 19:49:38 +0700

    feat: setup route, controller, and request

---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use App\Services\FrontService;
+use Illuminate\Http\Request;
+
+class FrontController extends Controller
+{
+    protected $frontService;
+
+    public function __construct(FrontService $frontService)
+    {
+        $this->frontService = $frontService;
+    }
+
+    public function index()
+    {
+        $data = $this->frontService->getFrontPageData();
+        return view('front.index', $data);
+    }
+
+    public function details(Product $product)
+    {
+        return view('front.details', compact('product'));
+    }
+}
