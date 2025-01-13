fb22088bdca2179102ad41bb7bb3ec580db13f18
novinbukannopin <novinardiany@outlook.com>
2025-01-13 19:28:31 +0700

    Fixed for css

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -4,14 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @stack('before-styles')
-    <link href="{{ asset('./output.css') }}" rel="stylesheet">
+    <link href="{{ asset('output.css') }}" rel="stylesheet">
     @stack('after-styles')
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap"
+          rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap"
+          rel="stylesheet"/>
 </head>
 <body>
-    @yield('content')
-    @stack('after-scripts')
+@yield('content')
+@stack('after-scripts')
 </body>
